df8354623cc27bff436637d29f9deea694f39a83
rvzko <ravza.korkmaz@rub.de>
2021-02-06 21:55:26 +0100

    06.02.2021 Ders sonu

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,13 +1,17 @@
-﻿using Entities.Concrete;
+﻿using Core.DataAccess;
+using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
-
+//Ürüne özel operasyonlar
 namespace DataAccess.Abstract
 {
     public interface IProductDal:IEntityRepository<Product>
     {
-        
+        List<ProductDetailDto> GetProductDetails();
     }
 }
+
+//Code Refactoring
