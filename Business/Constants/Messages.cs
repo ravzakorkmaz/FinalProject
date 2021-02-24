8b8fb38f485c81afbfbb140b3206ce9dc64c3bb1
rvzko <ravza.korkmaz@rub.de>
2021-02-24 22:08:08 +0100

    24.02.2021 ders sonu

--- Business/Constants/Messages.cs
+++ Business/Constants/Messages.cs
@@ -11,5 +11,8 @@ namespace Business.Constants
         public static string ProductNameInvalid = "Ürün ismi gecersiz";
         public static string MaintenanceTime = "Sistem bakimda";
         public static string ProductsListed = "Ürünler listelendi";
+        public static string ProductCountOfCategoryError = "Bir kategoride en fazla 10 ürün olabilir";
+        public static string ProductNameAlreadyExists = "Bu isimde zaten baska bir ürün var";
+        public static string CategoryLimitExceded = "Kategori limiti asildigi icin yeni ürün eklenemiyor";
     }
 }
